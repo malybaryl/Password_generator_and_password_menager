236d73dadc8731da0b55f1d95087250a31731983
malybaryl <kacper_barylowicz@wp.pl>
2022-10-20 15:20:05 +0200

    changes 1.0

--- password_generator_and_password_menager/password_generator_and_password_menager/password_generator_and_password_menager.cpp
+++ password_generator_and_password_menager/password_generator_and_password_menager/password_generator_and_password_menager.cpp
@@ -1,26 +1,35 @@
 #include <iostream>
 #include<cstdlib>
 #include<ctime>
-using namespace std;
-      
+
+
 int main()
 {
     const char alphanum[] = "0123456789!@#$%^&*abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     int string_length = sizeof(alphanum) - 1;
     int n, pass_gen;
-    cout << "What do you want to do?:" << endl;
-    cout << "1 - generate password" << endl;
-    cin >> pass_gen;
+    while (1)
+    {
+        std::cout << "What do you want to do?:" << std::endl;
+        std::cout << "1 - generate password" << std::endl;
+        std::cout << "2 - end" << std::endl;
+        std::cin >> pass_gen;
         if (pass_gen == 1)
         {
-        cout << "Enter the length of password:";
-        cin >> n;
-        srand(time(0));
-        for (int i = 0; i < n; i++)
-        cout << alphanum[rand() % string_length];
+            std::cout << "Enter the length of password:";
+            std::cin >> n;
+            srand(time(0));
+            for (int i = 0; i < n; i++)
+                std::cout << alphanum[rand() % string_length];
+            std::cout << std::endl;
         }
+        else if (pass_gen == 2)
+        {
+            exit(0);
+        }
+
+    }
     return 0;
 }
-    
 
-    
+
